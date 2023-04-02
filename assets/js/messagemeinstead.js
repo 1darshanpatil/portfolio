ef5bdf60acadb982e16fc5114af5f37ebc41b682
Darshan P <72539638+1darshanpatil@users.noreply.github.com>
2023-04-02 17:09:00 +0000

    Update messagemeinstead.js

--- assets/js/messagemeinstead.js
+++ assets/js/messagemeinstead.js
@@ -3,6 +3,6 @@ emailLink.addEventListener("click", (event) => {
   event.preventDefault();
   const email = event.target.textContent.trim();
   navigator.clipboard.writeText(email);
-  const message = `The email "${email}" has been copied to the clipboard. Sending a message is easier than email.`;
+  const message = `The email "${email}" has been copied to the clipboard. \nSending a message is easier than email.\nI'm sure you'll love message feature!`;
   window.alert(message);
 });
